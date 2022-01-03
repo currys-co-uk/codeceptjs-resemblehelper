73f99676d40b525d464f1c0e89cb8162017a83b1
Jan Travnicek <jan.travnicek@currys.co.uk>
2022-01-03 09:33:12 +0100

    make lint happy once more

--- index.ts
+++ index.ts
@@ -26,7 +26,8 @@ type Options = {
 
 type Selector = string | { shadow: string | string[] } | { css: string } | CodeceptJS.LocatorOrString;
 
-type GenericObject = { [key: string]: string | number };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type GenericObject = { [key: string]: any };
 
 type BoxCoordinates = { left: number; top: number; right: number; bottom: number };
 
